815c7a1ae29e30e3864d45713d5ec56f986601d1
Drazie <front.kd19@gmail.com>
2022-04-12 08:16:02 +0300

    Database done

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-
+using Core.Interfaces;
 
 namespace API
 {
@@ -21,7 +21,7 @@ namespace API
 
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
